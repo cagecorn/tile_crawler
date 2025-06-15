e46f895444e1b15928ddc1d3bd9980d1324832f5
cagecorn <lmcbv@naver.com>
2025-06-16 03:48:01 +0900

    Update entities.js

--- src/entities.js
+++ src/entities.js
@@ -1,14 +1,20 @@
-// 몬스터의 설계도 역할을 하는 Monster 클래스
+// src/entities.js
+
 export class Monster {
     constructor(x, y, tileSize) {
+        this.id = Math.random().toString(36).substr(2, 9); // 각 몬스터를 구별할 고유 ID
         this.x = x;
         this.y = y;
         this.width = tileSize / 2;
         this.height = tileSize / 2;
         this.color = 'red';
+        this.hp = 3; // 몬스터의 체력
     }
 
-    // 몬스터를 캔버스에 그리는 메서드
+    takeDamage(amount) {
+        this.hp -= amount;
+    }
+
     render(ctx) {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
