1d5558d7d349db8ab1ad124a1f83cc62d54c17ac
cagecorn <lmcbv@naver.com>
2025-06-18 01:40:58 +0900

    Add item system with equipment support

---
+++ src/data/items.js
@@ -0,0 +1,8 @@
+export const ITEMS = {
+    // 무기
+    short_sword: { name: '단검', type: 'weapon', tags: ['melee', 'sword'], imageKey: 'sword' },
+    long_bow: { name: '장궁', type: 'weapon', tags: ['ranged', 'bow'], imageKey: 'bow' },
+
+    // 방어구
+    leather_armor: { name: '가죽 갑옷', type: 'armor', tags: ['armor', 'light_armor'], imageKey: 'leather_armor' },
+};
